fc1be54830ba736fadd42890ddbb88c73569a155
juliomello93 <juliomello@live.com>
2023-03-22 15:00:16 -0300

    feat: change files to tailwindcss

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,6 +1,42 @@
-
-export default function Home() {
+const Home = () => {
   return (
-    <></>
+    <div className=" w-1/2 h-full p-6 flex flex-col justify-center m-auto mt-20">
+      <h1 className=" text-4xl text-white text-center mb-4">
+        Login
+      </h1>
+      <div className=" flex justify-center flex-col gap-1 content-center">
+        <label htmlFor="email" className="text-white text-sm">
+          Email
+        </label>
+        <input id="email" type="email" className="
+        rounded-sm 
+        bg-slate-500
+        m-auto
+        w-full
+        border
+        border-transparent
+        focus:outline-none
+        focus:border-sky-600   
+        "
+          placeholder="Digite seu e-mail"
+
+        />
+        <label htmlFor="password" className="text-white mt-2 text-sm">
+          Senha
+        </label>
+        <input type="password" placeholder="Digite sua senha" className="
+        rounded-sm 
+        bg-slate-500
+        m-auto
+        w-full
+        border
+        border-transparent
+        focus:outline-none
+        focus:border-sky-600        
+        " />
+      </div>
+    </div>
   )
 }
+
+export default Home
---
+++ frontend/src/pages/login.tsx
@@ -0,0 +1,7 @@
+const Login = () => {
+    return (
+        <h1>Pagina de Login</h1>
+    )
+}
+
+export default Login
