722b299fac69cc8c91c3c240a09ebe6b1b79ec57
juliomello93 <juliomello@live.com>
2023-05-15 15:18:44 -0300

    feat: change code and add files

--- frontend/README.md
+++ frontend/README.md
@@ -1,36 +1,28 @@
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
-## Techs
+## Getting Started
 
-```
-1. Next.JS
-2. TypeScript
-3. Prisma
-4. MongoDB
-5. TailwindCSS
-6. Axios
-7. NextAuth
-```
+First, run the development server:
 
-## Iniciando o seu projeto
-
-1. Faça o clone do repositório;
-
-```shell
-git clone url 
-```
-
-2. Instale as dependências necessárias:
-```shell
-yarn 
-```
-
-3. Para visualizar a aplicação:
-```shell
+```bash
+npm run dev
+# or
 yarn dev
+# or
+pnpm dev
 ```
 
-## Saiba mais:
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+
+You can start editing the page by modifying `pages/index.tsx`. The page auto-updates as you edit the file.
+
+[API routes](https://nextjs.org/docs/api-routes/introduction) can be accessed on [http://localhost:3000/api/hello](http://localhost:3000/api/hello). This endpoint can be edited in `pages/api/hello.ts`.
+
+The `pages/api` directory is mapped to `/api/*`. Files in this directory are treated as [API routes](https://nextjs.org/docs/api-routes/introduction) instead of React pages.
+
+This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
+
+## Learn More
 
 To learn more about Next.js, take a look at the following resources:
 
@@ -39,3 +31,8 @@ To learn more about Next.js, take a look at the following resources:
 
 You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
 
+## Deploy on Vercel
+
+The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
+
+Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,29 +1,34 @@
-import Head from 'next/head'
-import Link from 'next/link'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <div className=' flex justify-center'>
-      <Head>
-        <title>Projeto FullStack </title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-        <div className="px-4 py-6 sm:px-0">
-          <h1 className="text-3xl font-bold text-slate-100">Bem-vindo ao Projeto FullStack</h1>
-          <p className="mt-2 text-lg text-gray-600">Esta é a minha primeira página criada com Next.JS, Tailwindcss e Typescript.</p>
-          <div className="mt-6 space-x-4 flex justify-center">
-            <Link href="/login">
-              <button className=' bg-slate-400 rounded-sm text-blue-700 p-1 w-20'>Login</button>
-            </Link>
-            <Link href="/register">
-              <button className=' bg-slate-400 rounded-sm text-blue-700 p-1 w-20'>Cadastro</button>
-            </Link>
+    <main className={`flex flex-col items-center p-24 ${inter.className}`}>
+      <div className=' bg-slate-300 rounded-2xl shadow-2xl flex w-full'>
+        <div className=' w-3/5 p-5 border-r-4 border-sky-900 text-black'>
+          <p className=' text-black text-center mt-9 text-2xl'>Login</p>
+          <div className=' flex'>
+            <form action="" className=' flex flex-col w-full'>
+              <input type="email" placeholder='Email' className=' p-1 mb-3 mt-5 text-sm w-2/3 self-center rounded' />
+              <input type="password" placeholder='Senha' className=' p-1 mb-3 text-sm w-2/3 self-center rounded' />
+              <button className=' text-white rounded bg-sky-900 w-14 self-center mt-4'>Login</button>
+            </form>
           </div>
         </div>
-      </main>
-    </div>
+        <div className=' w-2/5 p-5 text-black'>
+          <p className=' text-black text-center text-2xl'>Cadastro</p>
+          <div className='flex justify-center flex-col'>
+            <form action="" className=' flex flex-col w-full'>
+              <input type="text" placeholder='Nome completo' className=' p-1 mt-5 text-sm rounded' />
+              <input type="text" placeholder='Telefone' className=' p-1 mt-3 text-sm rounded' />
+              <input type="email" placeholder='Email' className=' p-1 mb-3 mt-3 text-sm rounded' />
+              <input type="password" placeholder='Senha' className=' p-1 mb-5 text-sm rounded' />
+              <button className=' text-white rounded bg-sky-900 w-20 self-center'>Sign Up</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </main>
   )
 }
-
